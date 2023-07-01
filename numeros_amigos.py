f7f072c3ae75b1150b0b01fe1b9d9d9c95d997dd
Deibith1810 <deibithandre1810@gmail.com>
2023-06-30 22:15:45 -0500

    agregar

--- numeros_amigos.py
+++ numeros_amigos.py
@@ -1,13 +1,12 @@
 
-# Función para verificar si dos números son amigos
 def numeros_amigos(número1, número2):
     suma = 0  # Variable que sumará los divisores de un número
-
+    
     # Suma todos los divisores del número 1
     for i in range(1, número1):
         if número1 % i == 0:
             suma += i
-
+    
     # Si la suma de los divisores del número 1 es igual al número 2
     if suma == número2:
         suma = 0
@@ -15,7 +14,7 @@ def numeros_amigos(número1, número2):
         for i in range(1, número2):
             if número2 % i == 0:
                 suma += i
-
+        
         # Si la suma de los divisores de ambos números son iguales
         if suma == número1:
             return f"{número1} y {número2} son amigos"
@@ -24,11 +23,8 @@ def numeros_amigos(número1, número2):
     else:
         return f"{número1} y {número2} no son amigos"
 
-def main():
-    número1 = 220
-    número2 = 300
-    resultado = numeros_amigos(número1, número2)
-    print(resultado)
 
-if __name__ == "__main__":
-    main()
+
+
+
+
--- test_numeros_amigos.py
+++ test_numeros_amigos.py
@@ -1,5 +1,5 @@
 import numeros_amigos
 
-assert numeros_amigos.numeros_amigos(220, 284) == "220 y 284 no son amigos"
-
-print("La prueba ha fallado.")
+def test_numeros_amigos():
+    # Prueba que fallará
+    assert numeros_amigos.numeros_amigos(220, 284) == "220 y 284 no son amigos"
