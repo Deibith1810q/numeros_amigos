9060480bd0f84d7f21348de84b52ea1419bbcc60
Deibith1810 <deibithandre1810@gmail.com>
2023-06-30 22:54:07 -0500

    actualizacion7

--- numeros_amigos.py
+++ numeros_amigos.py
@@ -9,13 +9,22 @@ def numeros_amigos(número1, número2):
             suma += i
 
     # Si la suma de los divisores del número 1 es igual al número 2
+   def numeros_amigos(número1, número2):
+    suma = 0  # Variable que sumará los divisores de un número
+    
+    # Suma todos los divisores del número 1
+    for i in range(1, número1):
+        if número1 % i == 0:
+            suma += i
+    
+    # Si la suma de los divisores del número 1 es igual al número 2
     if suma == número2:
         suma = 0
         # Suma los divisores del número 2
         for i in range(1, número2):
             if número2 % i == 0:
                 suma += i
-
+        
         # Si la suma de los divisores de ambos números son iguales
         if suma == número1:
             return f"{número1} y {número2} son amigos"
@@ -23,12 +32,3 @@ def numeros_amigos(número1, número2):
             return f"{número1} y {número2} no son amigos"
     else:
         return f"{número1} y {número2} no son amigos"
-
-def main():
-    número1 = 220
-    número2 = 300
-    resultado = numeros_amigos(número1, número2)
-    print(resultado)
-
-if __name__ == "__main__":
-    main()
--- test_numeros_amigos.py
+++ test_numeros_amigos.py
@@ -1,5 +1,5 @@
 import numeros_amigos
 
-assert numeros_amigos.numeros_amigos(220, 284) == "220 y 284 no son amigos"
-
-print("La prueba ha fallado.")
+def test_numeros_amigos():
+    # Prueba exitosa
+    assert numeros_amigos.numeros_amigos(1184, 1210) == "1184 y 1210 no son amigos"
